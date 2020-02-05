fbbbda8ad22c9c8de830840823c80875b67ac436
Vijai Kalathur <kvijai@gmail.com>
2020-02-05 06:21:33 -0500

    Update package.sh

--- ci/package.sh
+++ ci/package.sh
@@ -33,13 +33,13 @@ done
 
 # build archive of pipelines
 tar -czf $assets_dir/kabanero-pipelines.tar.gz $pipelines_dir/*.yaml
-#echo -e "--- Created kabanero-pipelines.tar.gz"
-# build archive of pipelines
-cd $pipelines_dir
-cp $asset_manifest $pipelines_dir
-tar -czf default-kabanero-pipelines.tar.gz *.yaml
 echo -e "--- Created kabanero-pipelines.tar.gz"
-cp default-kabanero-pipelines.tar.gz $assets_dir
+# build archive of pipelines
+#cd $pipelines_dir
+#cp $asset_manifest $pipelines_dir
+#tar -czf default-kabanero-pipelines.tar.gz *.yaml
+#echo -e "--- Created kabanero-pipelines.tar.gz"
+#cp default-kabanero-pipelines.tar.gz $assets_dir
 
-cd $assets_dir
-ls
+#cd $assets_dir
+#ls
