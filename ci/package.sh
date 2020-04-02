b3a7bf5c19c5571fe775c821803d839791f1f4ac
Vijai Kalathur <kvijai@gmail.com>
2020-04-02 16:36:01 -0400

    Update package.sh

--- ci/package.sh
+++ ci/package.sh
@@ -56,11 +56,11 @@ done
 
 # build archive of pipelines
 tar -czf $assets_dir/default-kabanero-pipelines.tar.gz -C $pipelines_dir .
-sha256_default_tar_gz=$sha256cmd $assets_dir/default-kabanero-pipelines.tar.gz
-touch $assets_dir/default_tar_gz_sha256#$sha256_default_tar_gz
+sha256_default_tar_gz=$(($sha256cmd $assets_dir/default-kabanero-pipelines.tar.gz) | awk '{print $1}')
+touch $assets_dir/default-kabanero-pipelines-tar-gz_sha256#$sha256_default_tar_gz
 tar -czf $assets_dir/eventing-kabanero-pipelines.tar.gz -C $eventing_pipelines_dir .
-sha256_eventing_tar_gz=$sha256cmd $assets_dir/eventing-kabanero-pipelines.tar.gz
-touch $assets_dir/eventing_tar_gz_sha256#$sha256_eventing_tar_gz
+sha256_eventing_tar_gz=$(($sha256cmd $assets_dir/eventing-kabanero-pipelines.tar.gz) | awk '{print $1}')
+touch $assets_dir/eventing-kabanero-pipelines-tar-gz_sha256#$sha256_eventing_tar_gz
 echo -e "--- Created pipeline artifacts"
 
 # expose an extension point for running after main 'package' processing
